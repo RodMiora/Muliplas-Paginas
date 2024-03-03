bdfb4c7045e69b49e230b864d3d246619828f613
RodMiora <rodrigomiorin@gmail.com>
2024-03-02 23:24:57 -0300

    criando paginas

---
+++ PAGES/1_Cliente.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.write("Voce está na pagina cliente!")
+
+
---
+++ homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Seja bem vindo",
+    page_icon=":earth:"
+)
+
+st.sidebar.success("Escolha uma das opções!")
+
